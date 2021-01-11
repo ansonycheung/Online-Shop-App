838370b3a538b40f0baa13ad88aa63348e053271
anson <zhang.yang6@husky.neu.edu>
2021-01-10 16:16:05 -0800

    Spring Security Implementation.

--- src/main/java/onlineShop/SecurityConfig.java
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
-        .inMemoryAuthentication().withUser("stefanlaioffer@gmail.com").password("123").authorities("ROLE_ADMIN");
+        .inMemoryAuthentication().withUser("admin@shopping.com").password("admin").authorities("ROLE_ADMIN");
 
     auth
         .jdbcAuthentication()
